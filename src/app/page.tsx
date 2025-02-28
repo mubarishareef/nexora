da3f80349b4c281940397b46ca339ddfb781451b
connectlogistics <connectlogisticsuae@gmail.com>
2025-02-28 20:19:59 +0530

    Secon  commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -84,13 +84,13 @@ export default function Home() {
               arrowIcon
             >
               <Flex gap="8" vertical="center">
-                {about.avatar.display && (
+                {/* {about.avatar.display && (
                   <Avatar
                     style={{ marginLeft: "-0.75rem", marginRight: "0.25rem" }}
                     src={person.avatar}
                     size="m"
                   />
-                )}
+                )} */}
                 About us
               </Flex>
             </Button>
